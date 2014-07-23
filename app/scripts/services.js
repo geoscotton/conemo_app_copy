745f3f32d154a69fecf8811a95e852c8d1be14b8
Jeanie Chung <jeanie.chung@gmail.com>
2014-07-23 10:07:52 -0500

    Style changes to account for different phones, changed translation for portuguese contact alert.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,7 +2,7 @@
 
 angular.module('conemoAppApp')
   .factory('conemoConfig', ['$rootScope', function($rootScope) {
-    $rootScope.appVersion = "0.1.10";
+    $rootScope.appVersion = "0.1.11";
     
     function ConemoConfig() {}
 
